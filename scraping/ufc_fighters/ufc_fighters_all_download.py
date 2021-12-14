c6ac376e6802fd408f15d0caa4cc2205540c4450
Anant <anantarnav007@gmail.com>
2021-12-14 03:39:27 -0800

    added ufc figter extraction script

--- scraping/ufc_fighters/ufc_fighters_all_download.py
+++ scraping/ufc_fighters/ufc_fighters_all_download.py
@@ -19,16 +19,3 @@ def download_url(url, name):
 for i in ascii_lowercase:
   ufc_fighter = f"http://www.ufcstats.com/statistics/fighters?char={i}&page=all"
   download_url(ufc_fighter, i)
-
-# ufc_event_stats_url = "http://www.ufcstats.com/statistics/fighters?char=a&page=all"
-# _output_file = 'output.txt'
-# response = request.urlopen(ufc_event_stats_url)
-# web_content = response.read().decode('UTF-8')
-#
-# html_soup = BeautifulSoup(web_content, 'html.parser')
-#
-# row_container = html_soup.find_all('tr', class_='b-statistics__table-row')
-#
-# df = pd.DataFrame()
-#
-#
